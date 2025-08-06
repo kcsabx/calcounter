dc810534c6e32c43fc6f5d2ea9e14f27c651a9be
kcsabx <140919439+kcsabx@users.noreply.github.com>
2025-08-06 13:25:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from models import db, User
@@ -58,3 +59,26 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+
+@app.route('/api/calculate', methods=['POST'])
+def api_calculate():
+    data = request.get_json()
+    name = data['name']
+    weight = float(data['weight'])
+    height = float(data['height'])
+    age = int(data['age'])
+    gender = data['gender']
+    activity = float(data['activity'])
+    goal = data['goal']
+
+    bmr = calculate_bmr(gender, weight, height, age)
+    tdee = bmr * activity
+    recommended = tdee - 500 if goal == 'lose' else tdee
+
+    return jsonify({
+        'bmr': bmr,
+        'tdee': tdee,
+        'recommended_calories': recommended
+    })
+
